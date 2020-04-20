9f43c9d6ff5985ae2b68d5162bd4b043f0b58597
michaelchoquer <56238948+michaelchoquer@users.noreply.github.com>
2020-04-19 17:51:56 -0700

    First commit! Adding a type before I change from multiplying respective single-hv probability amplitudes from output and ideal to multiplying the joint probability amplitudes for the output together, squaring and using those instead (which is the correct approach)

---
+++ heralded_cnot.py
@@ -0,0 +1,61 @@
+import numpy as np
+
+# Why didn't I do this sooner?
+
+# Input states for (c, t) = (0,0) through (1,1)
+psi_input = np.array([[1, 1, 0, 0, 1, 1, 0], [1, 1, 0, 1, 0, 1, 0], [0, 1, 1, 1, 0, 1, 0],
+                      [0, 1, 1, 0, 1, 1, 0]])
+
+# Ideal output states for input above
+psi_ideal = np.array([[1, 1, 0, 0, 1, 1, 0], [1, 1, 0, 1, 0, 1, 0], [0, 1, 1, 0, 1, 1, 0],
+                      [0, 1, 1, 1, 0, 1, 0]])
+
+r = np.sqrt([0, 0.227, 1 / 2, 1 / 2, 0.227, 1 / 2, 0.243, 1 / 2])
+r = [np.random.normal(loc=r[i], scale=0.00 * r[i]) for i in range(len(r))]
+t = np.sqrt([1 - r[i] ** 2 for i in range(len(r))])
+
+def compute_overlap(p1, p2):
+    product = p1 * p2
+    return product if product < 1 else 1
+
+gate = np.array([[1, 0, 0, 0, 0, 0, 0],
+                 # c0
+                 [0, r[1], -r[2] * t[1], -r[3] * t[2] * t[1], t[3] * t[2] * t[1], 0, 0],
+                 # |1>
+                 [0, -t[1] * r[5], -r[2] * r[1] * r[5] - t[2] * r[4] * t[5],
+                  -r[3] * t[2] * r[1] * r[5] + r[3] * r[2] * r[4] * t[5],
+                  +t[3] * t[2] * r[1] * r[5] - t[3] * r[2] * r[4] * t[5], t[4] * t[5], 0],
+                 # c1
+                 [0, -t[1] * t[5] * t[6] * t[7], r[2] * r[1] * t[5] * t[6] * t[7] - t[2] * r[4] * r[5] * t[6] * t[7],
+                  -r[3] * r[2] * r[4] * r[5] * t[6] * t[7] - r[3] * t[2] * r[1] * t[5] * t[6] * t[7] + t[3] * r[7],
+                  t[3] * t[2] * r[1] * t[5] * t[6] * t[7] + r[3] * r[7] + t[3] * r[2] * r[4] * r[5] * t[6] * t[7],
+                  -t[4] * r[5] * t[6] * t[7], -r[6] * t[7]],
+                 # t1
+                 [0, t[1] * t[5] * t[6] * r[7], r[2] * r[1] * t[5] * t[6] * r[7] - t[2] * r[4] * r[5] * t[6] * r[7],
+                  +r[3] * t[2] * r[1] * t[5] * t[6] * r[7] + r[3] * r[2] * r[4] * r[5] * t[6] * r[7] + t[3] * t[7],
+                  -t[3] * t[2] * r[1] * t[5] * t[6] * r[7] - t[3] * r[2] * r[4] * r[5] * t[6] * r[7] + r[3] * t[7],
+                  t[4] * r[5] * t[6] * r[7], r[6] * r[7]],
+                 # t0
+                 [0, 0, t[2] * t[4], -r[3] * r[2] * t[4], t[3] * r[2] * t[4], r[4], 0],
+                 # |1>
+                 [0, t[1] * t[5] * r[6], r[2] * r[1] * t[5] * r[6] - t[2] * r[4] * r[5] * r[6],
+                  r[3] * r[2] * r[4] * r[5] * r[6] + r[3] * t[2] * r[1] * t[5] * r[6],
+                  -t[3] * t[2] * r[1] * t[5] * r[6] - t[3] * r[2] * r[4] * r[5] * r[6],
+                  t[4] * r[5] * r[6], -t[6]]])
+# |0>
+
+psi_out = np.array([np.matmul(gate, psi_input[j]) for j in range(len(psi_input))])
+heralding_prob = [np.abs(psi_out[i, 1] * psi_out[i, 5]) for i in range(len(psi_out))]
+fidelities = np.abs([compute_overlap(psi_out[0, 0], psi_ideal[0, 0]) *
+                     compute_overlap(psi_out[0, 4], psi_ideal[0, 4]),
+                     compute_overlap(psi_out[1, 0], psi_ideal[1, 0]) *
+                     compute_overlap(psi_out[1, 3], psi_ideal[1, 3]),
+                     compute_overlap(psi_out[2, 1], psi_ideal[2, 1]) *
+                     compute_overlap(psi_out[2, 4], psi_ideal[2, 4]),
+                     compute_overlap(psi_out[3, 1], psi_ideal[3, 1]) *
+                     compute_overlap(psi_out[3, 3], psi_ideal[3, 3])])
+fidelities = [fidelities[i] if fidelities[i] <= 1 else 1
+                          for i in range(len(psi_out))]
+
+a = [1 / heralding_prob[i] for i in range(len(psi_out))]
+print(fidelities)
